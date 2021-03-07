93f31a99e318831c3853ac08f42743eba94f18d3
Jacob Tellep <jj.tellep@gmail.com>
2021-03-06 22:19:23 -0500

    Learning about dropdown menus

--- client/src/components/ViewPricing.js
+++ client/src/components/ViewPricing.js
@@ -2,7 +2,13 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const ViewPricing = () => {
-  return <div className="container"></div>;
+  return (
+    <select>
+      <option value="Luke Skywalker">Luke Skywalker</option>
+      <option value="C-3PO">C-3PO</option>
+      <option value="R2-D2">R2-D2</option>
+    </select>
+  );
 };
 
 export default ViewPricing;
